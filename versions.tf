991d0e8f259ff0a04b873aa225b1a357d9f83e06
keskiju <jukka.keskiluopa@taitounited.fi>
2021-03-29 11:50:38 +0300

    chore: loosen azurerm provider version requirements

--- versions.tf
+++ versions.tf
@@ -18,7 +18,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=2.51.0"
+      version = ">=2.27.0"
     }
   }
   required_version = ">= 0.14"

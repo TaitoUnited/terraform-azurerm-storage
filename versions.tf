479a698282c2a77bc339ed8c4db1bffadb3f97cd
keskiju <jukka.keskiluopa@taitounited.fi>
2024-06-09 11:14:19 +0300

    feat: update versions

--- versions.tf
+++ versions.tf
@@ -18,9 +18,9 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">=3.8.0"
+      version = ">=3.107.0"
     }
   }
   
-  required_version = ">= 1.3.0"
+  required_version = ">= 1.8.0"
 }
